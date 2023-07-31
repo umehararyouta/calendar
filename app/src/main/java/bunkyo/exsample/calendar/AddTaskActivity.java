e82592964a16d1c0a5cdf5037430a0d876b1857c
umehara <c1p31018@gmail.com>
2023-07-31 11:33:58 +0900

    TimePickとDatePickを追加

--- app/src/main/java/bunkyo/exsample/calendar/AddTaskActivity.java
+++ app/src/main/java/bunkyo/exsample/calendar/AddTaskActivity.java
@@ -1,35 +1,45 @@
 package bunkyo.exsample.calendar;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import java.sql.Time;
 import java.util.Locale;
 
-public class AddTaskActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
-    private EditText editText;
+public class AddTaskActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+    private EditText datePickerItem;
+    private EditText timePickerItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tusk);
 
-        editText = findViewById(R.id.DatePickerItem);
+        datePickerItem = findViewById(R.id.DatePickerItem);
+        timePickerItem = findViewById(R.id.TimePickerItem);
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-        String str = String.format(Locale.US, "%d/%d/%d",year, monthOfYear+1, dayOfMonth);
-        editText.setText( str );
+        String tmp = String.format(Locale.US, "%d/%d/%d",year, monthOfYear+1, dayOfMonth);
+        datePickerItem.setText(tmp);
+        }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        String str = String.format(Locale.US, "%d:%d", hourOfDay, minute);
+        timePickerItem.setText( str );
 
     }
-
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePick();
         newFragment.show(getSupportFragmentManager(), "datePicker");
